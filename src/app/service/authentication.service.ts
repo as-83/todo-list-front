7fc6bb4bfce62b584ded1180baa97d07e7a92468
abdsul <abdsul3434@gmail.com>
2021-01-04 22:03:46 +0300

    Basic Authentication

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {User} from '../model/User';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) {
+  }
 
-  authenticate(username, password): boolean {
-    if (username === 'user' && password === '1234') {
-      sessionStorage.setItem('username', username);
-      return true;
-    } else {
-      return false;
-    }
+  authenticate(username, password): any {
+    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
+    return this.httpClient.get<User>('http://localhost:8080/todo-list/validateLogin', { headers }).pipe(
+      map(
+        userData => {
+          sessionStorage.setItem('username', username );
+          return userData;
+        }
+      )
+
+    );
   }
 
   isUserLoggedIn(): boolean {
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -9,7 +9,7 @@ import {AuthenticationService} from '../../service/authentication.service';
 })
 export class LoginComponent implements OnInit {
 
-  username = 'user';
+  username = '';
   password = '';
   invalidLogin = false;
 
@@ -20,13 +20,17 @@ export class LoginComponent implements OnInit {
   }
 
   checkLogin(): void {
-    if (this.loginservice.authenticate(this.username, this.password)
-    ) {
-      this.router.navigate(['']);
-      this.invalidLogin = false;
-    } else {
-      this.invalidLogin = true;
-    }
+    (this.loginservice.authenticate(this.username, this.password).subscribe(
+        data => {
+          this.router.navigate(['']);
+          this.invalidLogin = false;
+        },
+        error => {
+          this.invalidLogin = true;
+
+        }
+      )
+    );
   }
 
 }
